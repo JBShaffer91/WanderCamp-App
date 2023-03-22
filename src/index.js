55d776da95c31f6fda1801c578cf1ca33c632c91
Justin Shaffer <justinbshaffer91@gmail.com>
2023-03-21 18:51:43 -0700

    refactored dev environment, create basic layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-require('dotenv').config()
-const express = require('express');
-const locationController = require('./controllers/locationController');
-const weatherController = require('./controllers/weatherController');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const app = express();
-
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get('/location', locationController.getLocation);
-app.get('/weather', weatherController.getWeather);
-
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
