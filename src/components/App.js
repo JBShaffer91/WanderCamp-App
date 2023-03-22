87c70fa10f7657732a4c82bd352ee2cab75f3161
Justin Shaffer <justinbshaffer91@gmail.com>
2023-03-21 19:29:24 -0700

    create basic React app to display weather data

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { fetchWeatherData } from '../utils/weatherAPI';
+import { fetchLocationData } from '../utils/locationAPI';
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchUserWeatherData = async (query) => {
+    try {
+      const locationData = await fetchLocationData(query);
+      const { latitude, longitude } = locationData;
+      const fetchedWeatherData = await fetchWeatherData(latitude, longitude);
+      setWeatherData(fetchedWeatherData);
+    } catch (error) {
+      console.error('Error fetching user weather data:', error.message);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const query = event.target.elements.location.value;
+    fetchUserWeatherData(query);
+  };
+
+  return (
+    <div>
+      <h1>Green Outdoors Co</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="location" placeholder="Enter your location" />
+        <button type="submit">Get Weather</button>
+      </form>
+      {weatherData && (
+        <div>
+          <h2>{weatherData.location}</h2>
+          <p>
+            {weatherData.temperature}°F, {weatherData.description}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
